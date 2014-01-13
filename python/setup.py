40af355f6bf63597e8293d8a1c11fc99e9e834fd
Joe Hamman <jhamman@hydro.washington.edu>
2014-01-13 09:44:13 -0800

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ setup (name = 'nco',
   license = "GPLv2",
   description = """python bindings to NCO""",
   py_modules = ["nco"],
-  url = "",
+  url = "https://raw2.github.com/jhamman/nco-bindings/develop/python/nco.py",
   classifiers = [
         "Development Status :: 2 - Pre-Alpha",
         "Topic :: Utilities",
